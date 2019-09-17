214ac10af3c6e45c1b04f336afd933aa8a7ad9e8
kkw01234 <44748071+kkw01234@users.noreply.github.com>
2019-09-17 10:52:22 +0900

    doc: add 해야할일

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "rules": {
+        "no-console": 0,
+        "no-unused-vars": 0,
+        "node":true
+    }
+};
--- app.js
+++ app.js
@@ -1,9 +1,9 @@
+// eslint-disable-line global-require
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
