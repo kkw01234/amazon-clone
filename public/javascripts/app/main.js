db7586eea54262d3e37b34472e25700c48026290
kkw01234 <44748071+kkw01234@users.noreply.github.com>
2019-09-20 00:17:10 +0900

    refactor : 데이터 받아오는 것 모두 fetch로 변환

--- public/javascripts/app/main.js
+++ public/javascripts/app/main.js
@@ -1,75 +1,106 @@
-import {Carousel} from "./carousel.js";
+import { Carousel } from "./carousel.js";
 import data from "./data/data_rdb.js";
-import {SubContainer} from "./subcontainer.js";
+import { SubContainer } from "./subcontainer.js";
 import dummy from "./data/dummy.js";
-import {CardCategory} from "./cardcategory.js"
-import {MainContainer} from "./maincontainer.js";
-import {UrlImage} from "./carouselsource/urlimage.js";
-import {BottomCard} from "./carouselsource/bottom.js";
-import {EventEmitter} from "../eventemitter/eventemitter";
-import {footer} from "./footer.js";
-import {header} from "./header.js";
+import { CardCategory } from "./cardcategory.js"
+import { MainContainer } from "./maincontainer.js";
+import { UrlImage } from "./carouselsource/urlimage.js";
+import { BottomCard } from "./carouselsource/bottom.js";
+import { EventEmitter } from "../eventemitter/eventemitter";
+import { footer } from "./footer.js";
+import { header } from "./header.js";
 
 const rootContainer = {
-    root : document.querySelector("#root"),
-    init(){
-        this.root.innerHTML = "";
+    root: document.querySelector("#root"),
+    init() {
+
+        this.root.innerHTML = header.render();
         this.carouselemitter = new EventEmitter();
-        this.bottomCarousel = new Carousel({cards:[],width:50,emitter:this.carouselemitter,interval : false});
+        Promise.all([fetch("/data/maincard.json"), fetch("/data/bottomcarousel.json")]).then(async values => {
+            await this.makeCardCategory(values[0]);
+            await this.makeBottomCard(values[1]);
+            this.makeMainContainer();
+        }).then(() => {
+            Promise.all([fetch("/data/minicarousel.json"), fetch("/data/sub.json")]).then(async (values) => {
+                await this.makeMiniCarousel(values[0]);
+                await this.makeSubContainer(values[1]);
+            }).then(() => {
+                    this.root.innerHTML += this.maincontainer.render();
+                    this.root.innerHTML += this.subContainer.render();
+                    this.root.innerHTML += footer.render();
+                    this.enrollEvent();
+                });
+                
+        }).catch(err=>{
+
+        });
+      
+    },
+    async makeCardCategory(res) {
+        const json = await res.json();
         let circleCount = 0;
-        this.cards = dummy.main.reduce((prev,curr)=>{
+        this.cards = json.main.reduce((prev, curr) => {
             const cardcategory = new CardCategory({
-                                        title:curr.title,
-                                        backgroundColor:curr.color,
-                                        image:curr.image,
-                                        count:curr.button,
-                                        nowCount : circleCount,
-                                        emitter : this.carouselemitter
-                                        });
-            circleCount +=curr.button;
+                title: curr.title,
+                backgroundColor: curr.color,
+                image: curr.image,
+                count: curr.button,
+                nowCount: circleCount,
+                emitter: this.carouselemitter
+            });
+            circleCount += curr.button;
             prev.push(cardcategory);
             return prev;
-        },[]);
-        this.maincontainer = new MainContainer({cards:this.cards,bottomCarousel:this.bottomCarousel});
-        this.bottomcards = data.result.reduce((prev,curr,idx)=>{
+        }, []);
+    },
+    async makeBottomCard(res) {
+        const json = await res.json();
+        this.bottomcards = json.result.reduce((prev, curr, idx) => {
             prev.push(new BottomCard({
-                className : curr.type,
-                title : curr.title,
-                head : curr.head,
-                content : curr.body,
-                image : curr.image,
-                url : curr.link,
-                urlContent : curr.tail,
-                color : curr.color,
-                width : 50
+                className: curr.type,
+                title: curr.title,
+                head: curr.head,
+                content: curr.body,
+                image: curr.image,
+                url: curr.link,
+                urlContent: curr.tail,
+                color: curr.color,
+                width: 50
             }));
             return prev;
-        },[]);
-        this.bottomCarousel.cards = this.bottomcards;
-        const images = dummy.Mini.reduce((prev,curr)=>{
+        }, []);
+        this.bottomCarousel = new Carousel({ cards: this.bottomcards, width: 50, emitter: this.carouselemitter, interval: false });
+    },
+    async makeMainContainer() {
+        this.maincontainer = new MainContainer({ cards: this.cards, bottomCarousel: this.bottomCarousel });
+    },
+    async makeMiniCarousel(res) {
+        const json = await res.json();
+        console.log(json);
+        const images = json.mini.reduce((prev, curr) => {
             prev.push(new UrlImage(curr.image));
             return prev;
-        },[]);
-        this.miniCarousel = new Carousel({cards:images,width:11.25,height:0,title:"mini",interval:true});
-        this.subContainer = new SubContainer({carousel:this.miniCarousel,title:dummy.sub[0].title,content:dummy.sub[0].content,url:dummy.sub[0].url});
-        
-        // bottomCarousel.enrollEvent();
+        }, []);
+        this.miniCarousel = new Carousel({ cards: images, width: 11.25, height: 0, title: "mini", interval: true });
     },
-    render(){
-        this.root.insertAdjacentHTML("beforeend",this.maincontainer.render()); 
-        this.root.insertAdjacentHTML("afterbegin",header.render());
-        this.root.insertAdjacentHTML("beforeend",this.subContainer.render()); 
-        this.root.insertAdjacentHTML("beforeend",footer.render());
+    async makeSubContainer(res) {
+        const json = await res.json();
+        this.subContainer = new SubContainer({ carousel: this.miniCarousel, title: json.sub[0].title, content: json.sub[0].content, url: json.sub[0].url });
+    },
+    render() {
+        this.root.insertAdjacentHTML("afterbegin", header.render());
+        this.root.insertAdjacentHTML("beforeend", this.maincontainer.render());
+        this.root.insertAdjacentHTML("beforeend", this.subContainer.render());
+        this.root.insertAdjacentHTML("beforeend", footer.render());
         this.enrollEvent();
     },
-    enrollEvent(){
+    enrollEvent() {
         this.bottomCarousel.enrollEvent();
         this.miniCarousel.enrollEvent();
-        this.cards.forEach(value=>{
+        this.cards.forEach(value => {
             value.enrollEvent();
         });
     }
 }
 
 rootContainer.init();
-rootContainer.render();
