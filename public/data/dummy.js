6fa3039078b058d493cd4505765842c63de4982d
kkw01234 <44748071+kkw01234@users.noreply.github.com>
2019-09-19 20:21:10 +0900

    chore : data json 파일로 벼경

---
+++ public/data/dummy.js
@@ -0,0 +1,59 @@
+export default{
+    "sub":[
+        {
+            title : "Amazon Originals, exclusively on Prime Video",
+            content : `Prime Video is the only place where you can watch Amazon Original series like "The Marvelous Mrs. Maisel", "Tom Clancy's Jack Ryan", "Homecoming", and "The Man in the High Castle".`,
+            url_content : `Explore Prime Video`,
+            url : `https://www.amazon.com/gp/video/storefront/ref=dvm_us_aq_np_dhb_be_optorigt1?ie=UTF8&merchId=originals1`
+        }
+    ],
+    "main":[
+        {
+            title : `Ship`,
+            image : `https://images-na.ssl-images-amazon.com/images/G/01/marketing/prime/detail_page/InteractivePlane_TILES_SpriteSheet_A._CB513333159_.png`,
+            color: "#00A8E1",
+            button : 4
+        },
+        {
+            title : `Stream`,
+            image : `https://images-na.ssl-images-amazon.com/images/G/01/marketing/prime/detail_page/InteractivePlane_TILES_SpriteSheet_B._CB513285352_.png`,
+            color: "#FF6138",
+            button : 5
+        },
+        {
+            title : `Shop`,
+            image : `https://images-na.ssl-images-amazon.com/images/G/01/marketing/prime/detail_page/InteractivePlane_TILES_SpriteSheet_C._CB513285352_.png`,
+            color: "#A90067",
+            button : 4
+        },
+        {
+            title : `Read`,
+            image : `https://images-na.ssl-images-amazon.com/images/G/01/marketing/prime/detail_page/InteractivePlane_TILES_SpriteSheet_D._CB513285353_.png`,
+            color: "#36C2B4",
+            button : 2
+        },
+        {
+            title : `More`,
+            image : `https://images-na.ssl-images-amazon.com/images/G/01/marketing/prime/detail_page/InteractivePlane_TILES_SpriteSheet_E._CB513285352_.png`,
+            color : "#FFC400",
+            button : 2
+        }
+    ],
+    /**
+     * 
+     */
+    "Mini":[
+        {
+            image : "https://images-na.ssl-images-amazon.com/images/G/01/marketing/prime/detail_page/81WN._CB481226577_.jpg"
+        },
+        {
+            image: `https://images-na.ssl-images-amazon.com/images/G/01/marketing/prime/detail_page/91GWBxUa._CB482706877_.jpg`
+        },
+        {
+            image: `https://images-na.ssl-images-amazon.com/images/G/01/marketing/prime/detail_page/91yXd._CB482706877_.jpg`
+        },
+        {
+            image: `https://images-na.ssl-images-amazon.com/images/G/01/marketing/prime/detail_page/9159TQ-lLuL._CB461046093_.jpg`
+        }
+    ]
+}
