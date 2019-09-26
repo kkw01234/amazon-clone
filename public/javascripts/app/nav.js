ca831f95a44dc061f9df535171f4bd675385245e
kkw01234 <44748071+kkw01234@users.noreply.github.com>
2019-09-26 20:13:18 +0900

    chore : nav 주소 잘못쓰여있던거 변경

--- public/javascripts/app/nav.js
+++ public/javascripts/app/nav.js
@@ -7,8 +7,9 @@ export const nav={
         return /*html*/`
                 <nav class='admin-nav'>
                     <a href="/adminpage/user">전체 사용자 조회</a>
-                    <a href="/adminpage/main">메인(하단) 카로셀 변경</a>
-                    <a href="/adminpage/mini">미니 카로셀 변경</a>
+                    <a href="/adminpage/read/main">하단 아이템 조회</a>
+                    <a href="/adminpage/read/mini">미니 아이템 조회</a>
+                    <a href="/adminpage/write">아이템 추가</a>
                 </nav>
             `;
     },
