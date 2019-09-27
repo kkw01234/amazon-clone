7ac2d8a019d91ef3879eb7697ed5d30f1333218c
kkw01234 <44748071+kkw01234@users.noreply.github.com>
2019-09-27 09:21:36 +0900

    refactor : carouseldao 변수명 일부 수정

--- src/dao/carouseldao.js
+++ src/dao/carouseldao.js
@@ -1,23 +1,23 @@
 const { DBConnect } = require("../db/dbconnect");
 const carouselQuery = {
-    MAINCARDCAROUSEL: {
+    MAIN_CARD_CAROUSEL: {
         CREATE_TABLE: /*SQL*/`
         CREATE TABLE IF NOT EXISTS main_card_carousel(
             title VARCHAR(100) PRIMARY KEY,
             image VARCHAR(200) NOT NULL,
             color varchar(14) NOT NULL)
     `,
-        FINDALL: /*SQL*/`
+        FIND_ALL: /*SQL*/`
         SELECT main.title,main.image,main.color,count(main.title) as count,sequence FROM main_card_carousel as main, bottom_carousel as bottom WHERE main.title = bottom.title group by main.title order by main.sequence;
     `,
         INSERT: /*SQL*/`
             INSERT INTO main_card_carousel VALUES(?,?,?);
     `,
-        FINDTITLE : `SELECT title FROM main_card_carousel`,
+        FIND_TITLE : `SELECT title FROM main_card_carousel`,
 
     },
 
-    BOTTOMCAROUSEL: {
+    BOTTOM_CAROUSEL: {
         CREATE_TABLE: /*SQL*/`
         CREATE TABLE IF NOT EXISTS bottom_carousel(
             bottom_index int PRIMARY KEY AUTO_INCREMENT,
@@ -30,7 +30,7 @@ const carouselQuery = {
             color VARCHAR(14) NOT NULL
         )
         `,
-        FINDALL: /*SQL*/`
+        FIND_ALL: /*SQL*/`
             SELECT * FROM bottom_carousel, (SELECT title, sequence FROM main_card_carousel) as main where bottom_carousel.title = main.title order by main.sequence, bottom_index;
         `,
         INSERT: /*SQL*/`
@@ -41,7 +41,7 @@ const carouselQuery = {
         `
     },
 
-    MINICAROUSEL: {
+    MINI_CAROUSEL: {
         CREATE_TABLE: /*SQL */`
         CREATE TABLE IF NOT EXISTS mini_carousel(
             mini_index INT AUTO_INCREMENT PRIMARY KEY,
@@ -49,7 +49,7 @@ const carouselQuery = {
             image VARCHAR(200) NOT NULL
         )
         `,
-        FINDALL: /*SQL*/`
+        FIND_ALL: /*SQL*/`
             SELECT * FROM mini_carousel;
         `,
         INSERT: /*SQL*/`
@@ -60,7 +60,7 @@ const carouselQuery = {
         `
     },
 
-    SUBCONTAINER: {
+    SUB_CONTAINER: {
         CREATE_TABLE:/*SQL*/`
         CREATE TABLE IF NOT EXISTS sub_container(
             sub_index INT AUTO_INCREMENT PRIMARY KEY,
@@ -70,7 +70,7 @@ const carouselQuery = {
             url VARCHAR(200) NOT NULL
         )
         `,
-        FINDALL: /*SQL*/`
+        FIND_ALL: /*SQL*/`
             SELECT * FROM sub_container;
         `,
         INSERT: /*SQL*/`
@@ -96,7 +96,7 @@ class CarouselDAO {
         }
     }
     async findAll(){
-        return await DBConnect.query(carouselQuery[this.name].FINDALL);
+        return await DBConnect.query(carouselQuery[this.name].FIND_ALL);
     }
     async delete(index){
         const result = await DBConnect.query(carouselQuery[this.name].DELETE,[index]);
@@ -109,26 +109,26 @@ class CarouselDAO {
 
 class MainCardCarouselDAO extends CarouselDAO {
     constructor() {
-        super('MAINCARDCAROUSEL');
+        super('MAIN_CARD_CAROUSEL');
     }
     async findTitle(){
-        const result = await DBConnect.query(carouselQuery.MAINCARDCAROUSEL.FINDTITLE);
+        const result = await DBConnect.query(carouselQuery.MAIN_CARD_CAROUSEL.FIND_TITLE);
         return result;
     }
 }
 class BottomCarouselDAO extends CarouselDAO {
     constructor() {
-        super('BOTTOMCAROUSEL');
+        super('BOTTOM_CAROUSEL');
     }
 }
 class SubContainer extends CarouselDAO {
     constructor() {
-        super('SUBCONTAINER');
+        super('SUB_CONTAINER');
     }
 }
 class MiniCarousel extends CarouselDAO {
     constructor() {
-        super('MINICAROUSEL');
+        super('MINI_CAROUSEL');
     }
 }
 
@@ -140,7 +140,6 @@ mainCardCarousel.createTable();
 bottomCarousel.createTable();
 subContainer.createTable();
 miniCarousel.createTable();
-
 module.exports = {
     mainCardCarousel,
     bottomCarousel,
